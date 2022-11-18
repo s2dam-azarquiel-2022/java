7a7611f19240af37291dcb658c9d859cfd08d4a7
aruZeta <aru_hackZ.official@zohomail.eu>
2022-11-18 11:02:08 +0100

    [add] Flake with npm stuff

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake with a shell with npm and some utils";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    npm.url = "nixpkgs/eab71d47d511e3ae4f6e300c18c8436aa7316cdc";
+  };
+
+  outputs = { self, nixpkgs, npm }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      npmPkgs = npm.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          npmPkgs.nodePackages.npm
+          pkgs.nodejs
+          npmPkgs.nodePackages.vscode-langservers-extracted
+          npmPkgs.nodePackages.vscode-json-languageserver
+        ];
+      };
+    };
+}
