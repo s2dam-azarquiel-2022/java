44d4fc5e822be395f2a05d8a328de07a91b9beb1
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-01 16:29:37 +0100

    [refactor] Handle exceptions in servlets

--- Chistes/src/main/java/controller/connection/ConnectionHandler.java
+++ Chistes/src/main/java/controller/connection/ConnectionHandler.java
@@ -6,26 +6,17 @@ import java.sql.SQLException;
 import oracle.jdbc.pool.OracleConnectionPoolDataSource;
 
 public class ConnectionHandler {
-  public static Connection connect() {
+  public static Connection connect() throws SQLException {
     OracleConnectionPoolDataSource pool = null;
     Connection connection = null;
-    try {
-      pool = new OracleConnectionPoolDataSource();
-      pool.setURL(System.getenv("CHISTES_DB_URL"));
-      connection = pool.getConnection();
-    } catch (SQLException e) {
-      System.out.println("Connection start error");
-      e.printStackTrace();
-    }
+    pool = new OracleConnectionPoolDataSource();
+    pool.setURL(System.getenv("CHISTES_DB_URL"));
+    connection = pool.getConnection();
 
     return connection;
   }
 
-  public static void close(Connection connection) {
-    try { connection.close(); }
-    catch (SQLException e) {
-      System.out.println("Connection close error");
-      e.printStackTrace();
-    }
+  public static void close(Connection connection) throws SQLException {
+    connection.close();
   }
 }
