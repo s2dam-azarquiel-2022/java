7ca877e9d1ea276739ad3d9c3ecb55a06c41ad87
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-01 12:25:21 +0100

    [add] Chistes

---
+++ Chistes/src/main/java/controller/servlet/Root.java
@@ -0,0 +1,40 @@
+package controller.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Root extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  public Root() { super(); }
+
+  @Override
+  protected void doGet(
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    try {
+      ServletUtils.checkConnection(session);
+    } catch (Exception e) {
+      // TODO: 500 page
+      e.printStackTrace();
+    }
+
+    RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+    dispatcher.forward(request, response);
+  }
+
+  @Override
+  protected void doPost(
+    HttpServletRequest request, HttpServletResponse response
+  ) throws ServletException, IOException {
+    doGet(request, response);
+  }
+}
---
+++ Chistes/src/main/java/controller/servlet/ServletConfig.java
@@ -0,0 +1,15 @@
+package controller.servlet;
+
+import model.entity.Page;
+
+public class ServletConfig {
+  public static Page[] pages = {
+  };
+
+  public static enum sessionVars {
+    CONNECTION,
+  };
+
+  public static enum requestVars {
+  };
+}
