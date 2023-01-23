4dce4f906a2e136015cc387e8a072a135e48fb4b
aruZeta <aru_hackZ.official@zohomail.eu>
2023-01-23 14:28:45 +0100

    [add] Root servlet rounds and matches handling

--- FurboPorra/src/java/controller/servlet/Root.java
+++ FurboPorra/src/java/controller/servlet/Root.java
@@ -1,13 +1,16 @@
 package controller.servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.entity.Jornada;
+import model.entity.RoundSelectView;
+import controller.servlet.ServletConfig.ReqVars;
+import java.util.Collections;
 
 public class Root extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -20,6 +23,26 @@ public class Root extends HttpServlet {
     HttpServletResponse response
   ) throws ServletException, IOException {
     ServletUtils.servletTry(req, response, "/home.jsp", (sess, entityManager, dispatcher) -> {
+      ServletUtils.setIfNull(sess, ServletConfig.SessVars.ROUND_SELECT_VIEWS, () -> {
+        return entityManager
+          .createNamedQuery("Jornada.findAll", Jornada.class)
+          .getResultList()
+          .stream()
+          .map(RoundSelectView::toSelectView)
+          .collect(Collectors.toList());
+      });
+      Jornada selectedRound = entityManager.find(Jornada.class, Short.valueOf(
+        ServletUtils.updateIfNotNullAndGet(
+          sess,
+          req,
+          ServletConfig.SessVars.SELECTED_ROUND,
+          () -> { return "-1"; }
+        )
+      ));
+      req.setAttribute(
+        ReqVars.MATCH_LIST.name(),
+        selectedRound == null ? Collections.emptyList() : selectedRound.getPartidoList()
+      );
     });
   }
 
